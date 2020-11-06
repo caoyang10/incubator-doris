c5caadb646be1863f0ab025db43b02dca364a649
caoyang10 <535011125@qq.com>
2020-11-06 22:09:15 +0800

    [feature][routine load] support window feature in routine load

--- fe/fe-core/src/main/java/org/apache/doris/load/routineload/RoutineLoadJob.java
+++ fe/fe-core/src/main/java/org/apache/doris/load/routineload/RoutineLoadJob.java
@@ -106,6 +106,7 @@ public abstract class RoutineLoadJob extends AbstractTxnStateChangeCallback impl
     public static final long DEFAULT_MAX_BATCH_ROWS = 200000;
     public static final long DEFAULT_MAX_BATCH_SIZE = 100 * 1024 * 1024; // 100MB
     public static final long DEFAULT_EXEC_MEM_LIMIT = 2 * 1024 * 1024 * 1024L;
+    public static final long DEFAULT_WINDOW_INTERVAL_SEC = -1;
     public static final boolean DEFAULT_STRICT_MODE = false; // default is false
 
     protected static final String STAR_STRING = "*";
@@ -180,6 +181,8 @@ public abstract class RoutineLoadJob extends AbstractTxnStateChangeCallback impl
     protected long maxBatchIntervalS = DEFAULT_MAX_INTERVAL_SECOND;
     protected long maxBatchRows = DEFAULT_MAX_BATCH_ROWS;
     protected long maxBatchSizeBytes = DEFAULT_MAX_BATCH_SIZE;
+
+    protected long windowIntervalSec = DEFAULT_WINDOW_INTERVAL_SEC;
 
     /**
      * RoutineLoad support json data.
@@ -291,6 +294,9 @@ public abstract class RoutineLoadJob extends AbstractTxnStateChangeCallback impl
         }
         if (stmt.getExecMemLimit() != -1) {
             this.execMemLimit = stmt.getExecMemLimit();
+        }
+        if (stmt.getWindowIntervalSec() > 0) {
+            this.windowIntervalSec = stmt.getWindowIntervalSec();
         }
         jobProperties.put(LoadStmt.TIMEZONE, stmt.getTimezone());
         jobProperties.put(LoadStmt.STRICT_MODE, String.valueOf(stmt.isStrictMode()));
@@ -525,6 +531,10 @@ public abstract class RoutineLoadJob extends AbstractTxnStateChangeCallback impl
         return maxBatchSizeBytes;
     }
 
+    public long getWindowIntervalSec() {
+        return windowIntervalSec;
+    }
+
     public String getFormat() {
         String value = jobProperties.get(PROPS_FORMAT);
         if (value == null) {
@@ -748,6 +758,7 @@ public abstract class RoutineLoadJob extends AbstractTxnStateChangeCallback impl
             throw new MetaNotFoundException("db " + dbId + " does not exist");
         }
         planner = new StreamLoadPlanner(db, (OlapTable) db.getTable(this.tableId), this);
+        planner.setWindowIntervalSec(windowIntervalSec);
     }
 
     public TExecPlanFragmentParams plan(TUniqueId loadId, long txnId) throws UserException {
@@ -1326,6 +1337,7 @@ public abstract class RoutineLoadJob extends AbstractTxnStateChangeCallback impl
         jobProperties.put("execMemLimit", String.valueOf(execMemLimit));
         jobProperties.put("mergeType", mergeType.toString());
         jobProperties.put("deleteCondition", deleteCondition == null ? STAR_STRING : deleteCondition.toSql());
+        jobProperties.put("windowIntervalSec", String.valueOf(windowIntervalSec));
         jobProperties.putAll(this.jobProperties);
         Gson gson = new GsonBuilder().disableHtmlEscaping().create();
         return gson.toJson(jobProperties);
@@ -1380,6 +1392,7 @@ public abstract class RoutineLoadJob extends AbstractTxnStateChangeCallback impl
         out.writeLong(maxBatchIntervalS);
         out.writeLong(maxBatchRows);
         out.writeLong(maxBatchSizeBytes);
+        out.writeLong(windowIntervalSec);
         progress.write(out);
 
         out.writeLong(createTimestamp);
@@ -1427,6 +1440,9 @@ public abstract class RoutineLoadJob extends AbstractTxnStateChangeCallback impl
         maxBatchIntervalS = in.readLong();
         maxBatchRows = in.readLong();
         maxBatchSizeBytes = in.readLong();
+        if (Catalog.getCurrentCatalogJournalVersion() >= FeMetaVersion.VERSION_93) {
+            windowIntervalSec = in.readLong();
+        }
 
         switch (dataSourceType) {
             case KAFKA: {
@@ -1525,6 +1541,11 @@ public abstract class RoutineLoadJob extends AbstractTxnStateChangeCallback impl
         if (jobProperties.containsKey(CreateRoutineLoadStmt.MAX_BATCH_SIZE_PROPERTY)) {
             this.maxBatchSizeBytes = Long.valueOf(
                     jobProperties.remove(CreateRoutineLoadStmt.MAX_BATCH_SIZE_PROPERTY));
+        }
+
+        if (jobProperties.containsKey(CreateRoutineLoadStmt.WINDOW_INTERVAL_SEC_PROPERTY)) {
+            this.windowIntervalSec = Long.valueOf(
+                    jobProperties.remove(CreateRoutineLoadStmt.WINDOW_INTERVAL_SEC_PROPERTY));
         }
     }
 }
